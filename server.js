30a53710e50f24b76c8d368d3f39ca614a2555e2
harsh-97 <sethharsh.sureshchand2016@vitstudent.ac.in>
2017-09-03 20:24:57 +0530

    started db work

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var Pool = require('pg').Pool
 var app = express();
 
 app.use(morgan('combined'));
+var  pool = new Pool(config);
 
 var config = {
 	user: 'postgres',
@@ -83,36 +84,23 @@ app.get('/ui/main.js', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var  pool = new Pool(config);
-app.get('/test-db', function(req, res){
-	pool.query('select * from test', function(err, result){
-		if(err)
-		{
-			res.status(500).send(err.toString());
-		}
-		else
-		{
-			res.send(JSON.stringify(result));
-		}
-	});
-});
-
-cnt = 0;
-app.get('/counter', function(req, res){
-	cnt = cnt + 1;
-	res.send(cnt.toString());
-});
-
 app.get('/ui/style.css', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+cnt = 0;
+app.get('/counter', function(req, res){
+	cnt = cnt + 1;
+	res.send(cnt.toString());
 });
 
 var names =[];
@@ -122,15 +110,44 @@ app.get('/submit-name', function(req, res){
 	res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
-	var articleName = req.params.articleName
-	res.send(createTemplate(articles[articleName]));
+app.get('/test-db', function(req, res){
+	pool.query('select * from test', function(err, result){
+		if(err)
+		{
+			res.status(500).send(err.toString());
+		}
+		else
+		{
+			res.send(JSON.stringify(result.rows));
+		}
+	});
 });
+
+app.get('articles/:articleName', function(req, res){
+	var articleName = req.params.articleName
+	pool.query("select * from articles where title ='" + articleName + "'", function(err, result){
+		if(err)
+		{
+			res.status(500).send(err.toString());
+		}
+		else
+		{
+			if(result.rows.length === 0)
+			{
+				res.status(404).send("Article does not exist!")
+			}
+			else
+			{
+				res.send(createTemplate(result.rows[0]));
+			}
+		}
+	});
+});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
+var port = 2345;
 app.listen(port, function () {
 	console.log(`IMAD course app listening on port ${port}!`);
 });
